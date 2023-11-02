4fedfe743410b0b6799f1eda6f97941e5b5cca68
Bobbbbby <578094246@outlook.com>
2023-11-02 16:37:39 +0800

    fix bug

--- script/readTextFile.js
+++ script/readTextFile.js
@@ -1,28 +1,8 @@
 function readTextFile(file)
 {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === 4)
-        {
-            if (xhr.status === 200)
-            {
-                var content = xhr.responseText;
-                return content;
-            }
-            else if (xhr.status === 404)
-            {
-                var content = "# 404 Not Found";
-                return content;
-            }
-            else if (xhr.status === 500)
-            {
-                var content = "# 500 Internal Error";
-                return content;
-            }
-        }
-    }
-    xhr.open("GET", file, true);
-    xhr.send();
-    while (1) { }
+    var markdown = "";
+    fetch(file).then(res => {
+        markdown = res.text();
+    });
+    return markdown;
 }
