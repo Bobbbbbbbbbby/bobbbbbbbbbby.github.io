1fafc3efc9c01720cd640089121a2596d496f0fb
Bobbbbby <578094246@outlook.com>
2023-11-02 18:22:39 +0800

    test

--- script/show_list.js
+++ script/show_list.js
@@ -1,4 +1,4 @@
-var set_string =
+/*var set_string =
     '{' +
         '"sets":[' +
             '{"name":"Problem Solvings", "dir":"problem-solvings"},' +
@@ -19,4 +19,37 @@ function getListDiv() {
     }
     stringHTML = stringHTML + "</ul>";
     return stringHTML;
+}*/
+
+
+
+var baseUrl = "https://bobbbbbbbbbby.github.io/";
+var setsUrl = baseUrl + "blog_sets/";
+var setsListJsonUrl = setsUrl + "sets.json/"
+var scriptUrl = baseUrl + "script/";
+
+async function getListDiv()
+{
+    var obj = await readJsonFile(setsListJsonUrl);
+    var sets = obj.sets;
+    var count = obj.count;
+    var stringHTML = "<ul>";
+    for (var i = 0; i < count; i++)
+    {
+        stringHTML += "<li>" + sets[i].name + "</li>";
+    }
+    stringHTML += "</ul>";
+    return stringHTML;
+}
+
+async function readTextFile(file) {
+    var response = await fetch(file);
+    var markdown = await response.text();
+    return markdown;
+}
+
+async function readJsonFile(file) {
+    var response = await fetch(file);
+    var objFile = await response.json();
+    return objFile;
 }
