8f35a41a6ec97255056142fab68fa8891d589ab2
Bobbbbbbbbbby <xiongzijun20a@mails.ucas.ac.cn>
2023-11-09 16:04:21 +0800

    change button value

--- script/loadArticlePage.js
+++ script/loadArticlePage.js
@@ -5,7 +5,7 @@ function loadArticlePage()
     let element = document.getElementById("innerPage");
     innerPageContentSave = element.innerHTML;
     element.innerHTML = `<br><iframe id="innerFrame" title="Article Page" src="blog_articles.html"></iframe>`;
-    element.innerHTML += `<input id="goback" "type="button" onclick="reloadSave()" value="Go Back">`;
+    element.innerHTML += `<input id="goback" "type="button" onclick="reloadSave()" value="Index Page">`;
     let innerFrame = document.getElementById("innerFrame");
     innerFrame.width = window.innerWidth * 0.9;
     innerFrame.height = window.innerHeight - 40 - window.innerWidth * 0.2 * 0.212;
